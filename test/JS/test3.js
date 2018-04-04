81606100e6f722e53b69924a602c366026eea449
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2018-04-04 16:39:24 -0400

    Create test3.js

---
+++ test/JS/test3.js
@@ -0,0 +1,46 @@
+var num;
+
+function laugh(num)
+{
+    var w="";
+    for(var i=num;i>=1;i-- )
+    {
+        w+="ha";
+    }
+    w+="!";
+    return w;
+}
+
+console.log(laugh(3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
