d658b5d4df7f67b86aabbbc1ed5aa9d715870aff
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2018-04-06 13:05:57 -0400

    Update test4.js

--- test/test4.js
+++ test/test4.js
@@ -9,10 +9,6 @@ console.log(cry());
 
 
 
-/*
- * Programming Quiz: Inline Functions (5-6)
- */
-
 // don't change this code
 function emotions(myString, myFunc) {
     console.log("I am " + myString + ", " + myFunc(2));
@@ -47,10 +43,6 @@ for(var i=0;i<=2;i++)
 }
 
 
-
-/*
- * Programming Quiz: Joining the Crew (6-6)
- */
 
 var captain = "Mal";
 var second = "Zoe";
@@ -89,6 +81,19 @@ test.forEach(function(num, index, array){
 });
 
 console.log(test);
+
+
+
+var bills = [50.23, 19.12, 34.01, 100.11, 12.15, 9.90,
+29.11, 12.99, 10.00, 99.22, 102.20, 100.10, 6.77, 2.22];
+
+// your code goes here
+var totals = bills.map(function(bill) {
+  bill = bill * 0.15 + bill; 
+  return Number(bill.toFixed(2)); //--> toFixed() need toFixed(2) to show how many decimal. 
+});
+console.log(totals);
+
 
 
 
