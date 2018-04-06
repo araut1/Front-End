324a899c655196d0d82dd562e41e67e4db7a005d
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2018-04-06 13:50:27 -0400

    Update object.js

--- test/object.js
+++ test/object.js
@@ -33,3 +33,26 @@ var breakfast = {
 }
 
 console.log(breakfast); 
+
+
+
+var savingsAccount = {
+  balance: 1000,
+  interestRatePercent: 1,
+  deposit: function addMoney(amount) {
+    if (amount > 0) {
+      savingsAccount.balance += amount;
+    }
+  },
+  withdraw: function removeMoney(amount) {
+    var verifyBalance = savingsAccount.balance - amount;
+    if (amount > 0 && verifyBalance >= 0) {
+      savingsAccount.balance -= amount;
+    }
+  },
+  printAccountSummary() {
+   return("Welcome!\nYour balance is currently $"+savingsAccount.balance+" and your interest rate is "+ savingsAccount.interestRatePercent+"%."); 
+  },
+}; 
+
+savingsAccount.printAccountSummary();
