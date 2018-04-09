bfb1dcba101c28471564b7de2275d56e5de02b2b
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2018-04-09 16:18:10 -0400

    Update jQuery.js

--- test/jQuery.js
+++ test/jQuery.js
@@ -24,3 +24,19 @@ $("<div id=family2><h1>Family2</h1></div>").insertAfter("#family1");
 $("#family2").append("<div id=bruce><h2>Bruce</h2></div>");
 $("#bruce").append("<div id=madison><h3>Madison</h3></div>");
 $("#bruce").append("<div id=hunter><h3>Hunter<h3></div>");
+
+
+///////////////////////////////////////////////////////////////////////////
+
+function charCount()
+{
+   var t,n;
+
+ t = $(this).text();
+
+ n = t.length;
+
+ $(this).text(t + " " + n);
+}
+
+$("p").each(charCount);
