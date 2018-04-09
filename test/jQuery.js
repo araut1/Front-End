28a24dbda824d6a1e8870086dbaab89dde2def6d
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2018-04-09 16:06:52 -0400

    Update jQuery.js

--- test/jQuery.js
+++ test/jQuery.js
@@ -16,3 +16,11 @@ articleItems = $('.article-item');
 
 ul = articleItems.find('ul'); 
 ul.remove();
+
+
+//////////////////////////////////////////////////////////////////
+
+$("<div id=family2><h1>Family2</h1></div>").insertAfter("#family1");
+$("#family2").append("<div id=bruce><h2>Bruce</h2></div>");
+$("#bruce").append("<div id=madison><h3>Madison</h3></div>");
+$("#bruce").append("<div id=hunter><h3>Hunter<h3></div>");
