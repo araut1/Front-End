2bd7854e8b6333a9ae355e347ec862ff8d75750c
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2018-04-10 11:37:41 -0400

    Update eventlistner.js

--- eventlistner.js
+++ eventlistner.js
@@ -3,3 +3,13 @@ $( 'article' ).on( 'click', function() {
 });
 
 $( 'body' ).append( '<article> <h1>Appended Article</h1> <p>Content for the new article </p> </article>' );
+
+
+
+$("#my-button").click(function () {
+
+            $("#my-button").remove();
+            $('body').addClass('success');
+
+        }
+    );
